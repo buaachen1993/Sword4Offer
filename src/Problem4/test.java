2ae2f87eab2ddd1b1566ac61f1f38a3fc793bee2
陈兴岳 <414731550@qq.com>
2017-03-07 09:43:39 +0800

    Problem4

---
+++ src/Problem4/test.java
@@ -0,0 +1,10 @@
+package Problem4;
+
+public class test {
+    public static void main(String args[]){
+		ReplaceBlank test=new ReplaceBlank();
+		String s=" // aa    ";
+		System.out.println(test.replaceBlank(s));
+    }
+
+}
