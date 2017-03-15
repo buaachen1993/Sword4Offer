00361d9cee8101d301da16f84a630b06c616474f
陈兴岳 <414731550@qq.com>
2017-03-15 17:03:11 +0800

    Problem8

---
+++ src/Problem8/test.java
@@ -0,0 +1,9 @@
+package Problem8;
+
+public class test {
+	public static void main(String args[]) throws Exception{
+		Min testMin = new Min();
+		int[] array = {1,1,0,1,1};
+		System.out.println(testMin.minInReversingList(array));
+	}
+}
