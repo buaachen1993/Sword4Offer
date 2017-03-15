674b1d9918e487427bbcc6d21c2384a8785bcc41
陈兴岳 <414731550@qq.com>
2017-03-15 19:24:50 +0800

    Problem 10 位运算

---
+++ src/Problem10/NumberOf1InBinary.java
@@ -0,0 +1,17 @@
+package Problem10;
+/***
+ * 求二进制的数字1的个数
+ * @author chandler
+ * 一个数减一和原来做与运算 相当于将最后一个1变成0
+ */
+public class NumberOf1InBinary {
+	public int NumberOf1(int n)
+	{
+		int count = 0;
+		while(n!=0){
+			count++;
+			n = (n-1) & n;
+		}
+		return count;
+	}
+}
---
+++ src/Problem10/test.java
@@ -0,0 +1,9 @@
+package Problem10;
+
+public class test {
+    public static void main(String args[]){
+    	NumberOf1InBinary test = new NumberOf1InBinary();
+    	System.out.println(test.NumberOf1(5));
+    }
+
+}
