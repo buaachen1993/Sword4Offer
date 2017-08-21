cbd52eba20d3623a2439ab09a543e88fe9deead9
陈兴岳 <414731550@qq.com>
2017-08-21 15:28:16 +0800

    P30_KLeastNumbers

---
+++ src/P30_KLeastNumbers/KLeastNumbers.java
@@ -0,0 +1,40 @@
+package P30_KLeastNumbers;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class KLeastNumbers {
+    public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+
+    		ArrayList<Integer> result = new ArrayList<Integer>();
+    		int length = input.length;
+    		if(k > length || k == 0){
+    			return result;
+    		}
+    		PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(k, new Comparator<Integer>() {
+
+				@Override
+				public int compare(Integer o1, Integer o2) {
+					// TODO Auto-generated method stub
+    				return o2.compareTo(o1);
+				}
+			});
+    		//维护一个代表最小值的容器
+   
+    		for (int i = 0; i < length; i++) {
+    			if (maxHeap.size() != k) {
+    				maxHeap.offer(input[i]);
+    			} else if (maxHeap.peek() > input[i]) {
+    				Integer temp = maxHeap.poll();
+    				temp = null;
+    				maxHeap.offer(input[i]);
+    			}
+    		}
+    		for (Integer integer : maxHeap) {
+    			result.add(integer);
+    		}
+    		return result;
+    }       
+}
+
